0279bbb7d58ad057f42c547c7717220817af0a99
sarvon77 <sarvon77@hotmail.com>
2018-01-06 05:19:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ io.on('connection', function (socket) {
 	   io.to(ambulanceId[amId[0]]).emit("emergency", data);
   });
   socket.on('serviceAccept', function (data) {
-	  console.log("serviceAccept",data.data.contact);
-	  //for(var f = 0;f < data.data.contact;f ++){
-	  // io.to(clientSocketId[data.data.contact[f]]).emit("emergencyServiceAccept", data);
-	  //}
+	  var contactDetails = data.data.contact;
+	  for(var f = 0;f < contactDetails.length;f ++){
+	   io.to(clientSocketId[contactDetails[f]]).emit("emergencyServiceAccept", data);
+	  }
   });
 	
 });
