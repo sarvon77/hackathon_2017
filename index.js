3b71e7afa517f174699008bb6b485b4e2392ed3d
sarvon77 <sarvon77@hotmail.com>
2018-01-06 05:14:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ io.on('connection', function (socket) {
 	   io.to(ambulanceId[amId[0]]).emit("emergency", data);
   });
   socket.on('serviceAccept', function (data) {
-	  console.log("serviceAccept",data.data.contact);
+	  console.log("serviceAccept",data.data);
 	  //for(var f = 0;f < data.data.contact;f ++){
 	  // io.to(clientSocketId[data.data.contact[f]]).emit("emergencyServiceAccept", data);
 	  //}
