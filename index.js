2738d0eb2516c1d7f09230bb48ee2ccd8e50be56
sarvon77 <sarvon77@hotmail.com>
2018-01-06 04:47:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,12 @@ io.on('connection', function (socket) {
 	   console.log(data,amId,ambulanceId);
 	   io.to(ambulanceId[amId[0]]).emit("emergency", data);
   });
+  socket.on('serviceAccept', function (data) {
+	  for(var f = 0;f < data.data.contact;f ++){
+	   io.to(clientSocketId[data.data.contact[f]]).emit("emergencyServiceAccept", data);
+	  }
+  });
+	
 });
 console.log("SDasdSA");
 /*
